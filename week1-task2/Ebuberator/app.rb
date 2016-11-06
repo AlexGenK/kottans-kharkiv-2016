fffbc0b78807f1853ec611873e6ca3d928d550ce
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-11-06 12:34:58 +0200

    create Ebuberable class

--- week1-task2/Ebuberator/app.rb
+++ week1-task2/Ebuberator/app.rb
@@ -1,1 +1,48 @@
+module Ebuberable
 
+  def map(&block)
+    out=[]
+    each {|item| out << block.call(item)}
+    out
+  end
+
+  def select(&block)
+    out=[]
+    each {|item| out << item if block.call(item)}
+    out
+  end
+
+  def reject(&block)
+    out=[]
+    each {|item| out << item unless block.call(item)}
+    out
+  end
+
+  def grep(pattern)
+    out=[]
+    each {|item| out << item if pattern===item}
+    out
+  end
+
+  def all?(&block)
+    block||=proc{|item| item}
+    each {|item| return false unless block.call(item)}
+    true
+  end
+
+  def reduce(param1=nil, param2=nil, &block)
+    if block==nil
+      if param2==nil
+        sym, accum = param1, param2
+      else
+        sym, accum = param2, param1
+      end
+      each {|item| accum ? accum=accum.send(sym, item) : accum=item}
+    else
+      accum = param1
+      each {|item| accum ? accum=block.call(accum, item) : accum=item}
+    end
+    accum
+  end
+
+end
---
+++ week1-task2/Ebuberator/app_spec.rb
@@ -0,0 +1,59 @@
+require './app.rb'
+
+class FiveLittlePigs
+
+  def each
+    yield 'Ниф-Ниф'
+    yield 'Наф-Наф'
+    yield 'Нуф-Нуф'
+    yield 'Ноф-Наф'
+    yield 'Ниф+Няф'
+  end
+
+  include Ebuberable
+
+end
+
+describe Ebuberable do
+
+  it "has realised map method" do
+    z=FiveLittlePigs.new.map{|i| "Поросенок #{i}"}
+    expect(z).to eq ['Поросенок Ниф-Ниф', 'Поросенок Наф-Наф', 'Поросенок Нуф-Нуф', 'Поросенок Ноф-Наф', 'Поросенок Ниф+Няф']
+    z=FiveLittlePigs.new.map(&:reverse)
+    expect(z).to eq ['фиН-фиН', 'фаН-фаН', 'фуН-фуН', 'фаН-фоН', 'фяН+фиН']
+  end
+
+  it "has realised select method" do
+    z=FiveLittlePigs.new.select{|i| i.include?'Наф'}
+    expect(z).to eq ['Наф-Наф', 'Ноф-Наф']
+  end
+
+  it "has realised reject method" do
+    z=FiveLittlePigs.new.reject{|i| i.include?'Наф'}
+    expect(z).to eq ['Ниф-Ниф', 'Нуф-Нуф', 'Ниф+Няф']
+  end
+
+  it "has realised grep method" do
+    z=FiveLittlePigs.new.grep(/^Наф/)
+    expect(z).to eq ['Наф-Наф']
+  end
+
+  it "has realised all? method" do
+    z=FiveLittlePigs.new.all?
+    expect(z).to eq true
+    z=FiveLittlePigs.new.all?{|i| /-/===i}
+    expect(z).to eq false
+  end
+
+  it "has realised reduce method" do
+    z=FiveLittlePigs.new.reduce(:+)
+    expect(z).to eq 'Ниф-НифНаф-НафНуф-НуфНоф-НафНиф+Няф'
+    z=FiveLittlePigs.new.reduce('Суммарный поросенок ', :+)
+    expect(z).to eq 'Суммарный поросенок Ниф-НифНаф-НафНуф-НуфНоф-НафНиф+Няф'
+    z=FiveLittlePigs.new.reduce("Список поросят -") {|accum, item| accum+" "+item}
+    expect(z).to eq 'Список поросят - Ниф-Ниф Наф-Наф Нуф-Нуф Ноф-Наф Ниф+Няф'
+    z=FiveLittlePigs.new.reduce{|accum, item| accum+" "+item}
+    expect(z).to eq 'Ниф-Ниф Наф-Наф Нуф-Нуф Ноф-Наф Ниф+Няф'
+  end
+
+end
