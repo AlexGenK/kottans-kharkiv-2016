421ae4bb2d054125cb553727c37be345ca7592b1
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-11-15 15:53:54 +0200

    refactoring reduce method

--- week1-task2/Ebuberator/app.rb
+++ week1-task2/Ebuberator/app.rb
@@ -31,9 +31,8 @@ module Ebuberable
   end
 
   def reduce(*args, &block)
-    accum=nil
-    block=args.last.to_proc if args.last.is_a? Symbol
-    accum=args.first unless args.first.is_a? Symbol
+    block=args.pop.to_proc if args.last.is_a? Symbol
+    accum=args.shift
     each {|item| accum ? accum=block.call(accum, item) : accum=item}
     accum
   end
